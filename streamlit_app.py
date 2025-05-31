4808712aee5e55e6a0c6e59a3cdf88ef49639e22
Larissa Araujo <162187647+LariAraujo1@users.noreply.github.com>
2025-05-31 19:50:32 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 # Import python packages
-import streamlit as st
+#import streamlit as st
 from snowflake.snowpark.functions import col
 streamlit.title('My Parents New Healthy Diner')
 streamlit.subtitle('Breakfast Menu')
